55f9f127ca40ebc92f6f36453aeef899e1aafbcc
msalgado008 <37191572+msalgado008@users.noreply.github.com>
2019-11-26 14:50:28 -0600

    Cambio de layout

---
+++ EPROCUREMENT.GAPPROVEEDOR.Entities/Catalogo/EstatusProveedorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPROCUREMENT.GAPPROVEEDOR.Entities
+{
+    public class EstatusProveedorDTO
+    {
+        public int IdEstatusProveedor { get; set; }
+        public string Estatus { get; set; }
+        public string EstatusDescripcion { get; set; }  
+    }
+}
